a1a9c87ff65084c23eea64751b0f5b8a83fd1fde
Giovanni Cardamone <g.cardamone2@gmail.com>
2020-09-11 20:21:24 +0200

    initial schema declaration

---
+++ src/index.ts
@@ -0,0 +1,110 @@
+export type JsonSchema =
+  | StringJsonSchema
+  | NumberJsonSchema
+  | ObjectJsonSchema
+  | ArrayJsonSchema
+  | BooleanJsonSchema
+  | NullJsonSchema
+
+export type JsonSchemaProperty =
+  | StringJsonSchemaProperty
+  | NumberJsonSchemaProperty
+  | ObjectJsonSchemaProperty
+  | ArrayJsonSchemaProperty
+  | BooleanJsonSchemaProperty
+  | NullJsonSchemaProperty
+
+export type JsonPropertiesTypes = 'string' | 'number' | 'object' | 'array' | 'boolean' | 'null'
+
+interface BaseJsonSchema {
+  $schema?: string
+  $id?: string
+  title?: string
+  description?: string
+  required?: string[]
+}
+
+interface BaseJsonSchemaProperty {
+  description?: string
+}
+
+interface StringJsonSchemaProperty extends BaseJsonSchemaProperty {
+  type: 'string'
+  minLength?: number
+  maxLength?: number
+  pattern?: string
+  format?:
+    | 'date-time'
+    | 'time'
+    | 'date'
+    | 'email'
+    | 'idn-email'
+    | 'hostname'
+    | 'idn-hostname'
+    | 'ipv4'
+    | 'ipv6'
+    | 'uri'
+    | 'uri-reference'
+    | 'iri'
+    | 'iri-reference'
+    | 'uri-template'
+    | 'json-pointer'
+    | 'relative-json-pointer'
+    | 'regex'
+    | 'string'
+}
+
+interface NumberJsonSchemaProperty extends BaseJsonSchemaProperty {
+  type: 'number'
+  multipleOf?: number
+  minimum?: number
+  exclusiveMinimum?: number
+  maximum?: number
+  exclusiveMaximum?: number
+}
+
+interface ObjectJsonSchemaProperty extends BaseJsonSchemaProperty {
+  type: 'object'
+  properties: {
+    [key: string]: JsonSchemaProperty
+  }
+  additionalProperties?: boolean
+  required?: string[]
+  propertyNames?: {
+    pattern?: string
+  }
+  minProperties?: number
+  maxProperties?: number
+  dependencies?: {
+    [key: string]: string[] | JsonSchemaProperty
+  }
+  patternProperties?: {
+    [key: string]: { type: JsonPropertiesTypes }
+  }
+}
+
+interface ArrayJsonSchemaProperty extends BaseJsonSchemaProperty {
+  type: 'array'
+  items: JsonSchemaProperty | JsonSchemaProperty[]
+  contains?: {
+    type: JsonPropertiesTypes
+  }
+  minItems?: number
+  maxItems?: number
+  uniqueItems?: boolean
+}
+
+interface BooleanJsonSchemaProperty extends BaseJsonSchemaProperty {
+  type: 'boolean'
+}
+
+interface NullJsonSchemaProperty extends BaseJsonSchemaProperty {
+  type: 'null'
+}
+
+type StringJsonSchema = BaseJsonSchema & StringJsonSchemaProperty
+type NumberJsonSchema = BaseJsonSchema & NumberJsonSchemaProperty
+type ObjectJsonSchema = BaseJsonSchema & ObjectJsonSchemaProperty
+type ArrayJsonSchema = BaseJsonSchema & ArrayJsonSchemaProperty
+type BooleanJsonSchema = BaseJsonSchema & BooleanJsonSchemaProperty
+type NullJsonSchema = BaseJsonSchema & NullJsonSchemaProperty
